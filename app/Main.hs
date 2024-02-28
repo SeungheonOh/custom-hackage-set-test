85c1933416328bfaf21c93c32c755fda70b86a35
Seungheon Oh <seungheon.ooh@gmail.com>
2024-02-27 20:27:05 -0600

    In the beginning there was darkness

---
+++ app/Main.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE TypeOperators #-}
+
+module Main where
+
+import Plutarch
+import Plutarch.Prelude
+import Plutarch.Api.V2
+import PlutusLedgerApi.Common
+import Data.Default
+import qualified Data.ByteString.Base16 as Base16
+import qualified Data.ByteString.Short as SBS
+import qualified Data.Text.Encoding as Text
+
+alwaysPass :: Term s (PInteger :--> PMintingPolicy)
+alwaysPass = plam $ \_ _ _ -> popaque $ pconstant ()
+
+main :: IO ()
+main =
+  case compile def alwaysPass of
+    Right scr@(Script x) -> do
+      print $ Text.decodeUtf8 $ Base16.encode $ SBS.fromShort $ serialiseUPLC x
+      print $ scriptHash scr
+    _ -> putStrLn "no"
---
+++ flake.nix
@@ -0,0 +1,75 @@
+{
+  description = "plutarch";
+
+  nixConfig = {
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+    allow-import-from-derivation = "true";
+    bash-prompt = "\\[\\e[0m\\][\\[\\e[0;2m\\]nix \\[\\e[0;1m\\]plutarch \\[\\e[0;93m\\]\\w\\[\\e[0m\\]]\\[\\e[0m\\]$ \\[\\e[0m\\]";
+    cores = "1";
+    max-jobs = "auto";
+    auto-optimise-store = "true";
+  };
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+
+    haskell-nix.url = "github:input-output-hk/haskell.nix";
+    iohk-nix.url = "github:input-output-hk/iohk-nix";
+    iohk-nix.inputs.nixpkgs.follows = "haskell-nix/nixpkgs";
+
+    CHaP.url = "github:input-output-hk/cardano-haskell-packages?ref=repo";
+    CHaP.flake = false;
+
+    custom-hackage-set.url = "github:seungheonoh/custom-hackage-set?ref=test";
+    custom-hackage-set.flake = false;
+
+    hercules-ci-effects.url = "github:hercules-ci/hercules-ci-effects";
+  };
+
+  outputs = inputs@{ flake-parts, nixpkgs, haskell-nix, iohk-nix, CHaP, custom-hackage-set, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+      ];
+      debug = true;
+      systems = [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ];
+      #hercules-ci.github-pages.branch = "master";
+
+      perSystem = { config, system, lib, self', ... }:
+        let
+          pkgs =
+            import haskell-nix.inputs.nixpkgs {
+              inherit system;
+              overlays = [
+                haskell-nix.overlay
+                iohk-nix.overlays.crypto
+                iohk-nix.overlays.haskell-nix-crypto
+              ];
+              inherit (haskell-nix) config;
+            };
+          project = pkgs.haskell-nix.cabalProject' {
+            src = ./.;
+            compiler-nix-name = "ghc964";
+            index-state = "2024-01-16T11:00:00Z";
+            inputMap = {
+              "https://input-output-hk.github.io/cardano-haskell-packages" = CHaP;
+              "https://seungheonoh.github.io/custom-hackage-set" = custom-hackage-set;
+            };
+            shell = {
+              withHoogle = true;
+              withHaddock = true;
+              exactDeps = false;
+              tools = {
+                cabal = { };
+              };
+            };
+          };
+          flake = project.flake { };
+        in
+        {
+          inherit (flake) devShells;
+        };
+    };
+}
